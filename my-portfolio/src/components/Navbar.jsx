3612e76185d0b219a45d63f0a5f421a21f589ad8
ananyab1909 <bananya1909@gmail.com>
2024-08-02 14:40:30 +0530

    portfolio updated

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,37 +1,33 @@
 import React from "react";
+import "./Navbar.css";
 
-
-const Navbar = () => {
-    // const [nav,setNav] = useState(false)
-    // const handleNav = () => {
-    //     setNav(!nav)
-    // }
+const Navbar = ({ onButtonClick }) => {
   return (
-    <div className = 'flex justify-center items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-        <ul className="flex flex-row flex-wrap justify-center">
-            <li className="p-2">Home</li>
-            <li className="p-2">About</li>
-            <li className="p-2">Skills</li>
-            <li className="p-2">Socials</li>
-            <li className="p-2">Contact</li>
-        </ul>
-        {/* <div onClick={handleNav} className="block md:hidden">
-            {!nav ? <AiOutlineClose size={20}/> :<AiOutlineMenu size={20}/>}
-            <AiOutlineMenu size = {20} />
-        </div> */}
-        {/* <div className={'fixed left-0 top-0 w-[60%] border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'}>
-            <h1 className="w-full text-3xl font-bold text-[#00df9a] m-8">REACT.</h1>
-            <ul className="pt-24 uppercase">
-                <li className="p-4 border-b border-gray-600">Home</li>
-                <li className="p-4 border-b border-gray-600">Company</li>
-                <li className="p-4 border-b border-gray-600">Resources</li>
-                <li className="p-4 border-b border-gray-600">About</li>
-                <li className="p-4">Contact</li>
-            </ul>
-        </div> */}
+    <div className="flex justify-center items-center h-24 max-w-[1240px] mx-auto px-4 text-white mt-0">
+      <ul className="flex flex-row flex-wrap justify-center gap-1 md:gap-2 lg:gap-4">
+        <li className="p-2 md:p-4 font-normal text-xm md:text-xm lg:text-xl font-serif hover: transition duration-300 ease-in-out ">
+          <button onClick={() => onButtonClick("Home")} className="nav-link">
+            Home
+          </button>
+        </li>
+        <li className="p-2 md:p-4 font-normal text-xm md:text-xm lg:text-xl font-serif hover: transition duration-300 ease-in-out">
+          <button onClick={() => onButtonClick("about")} className="nav-link">
+            About
+          </button>
+        </li>
+        <li className="p-2 md:p-4 font-normal text-xm md:text-xm lg:text-xl font-serif hover: transition duration-300 ease-in-out">
+          <button onClick={() => onButtonClick("Skills")} className="nav-link">
+            Skills
+          </button>
+        </li>
+        <li className="p-2 md:p-4 font-normal text-xm md:text-xm lg:text-xl font-serif hover: transition duration-300 ease-in-out">
+          <button onClick={() => onButtonClick("Contact")} className="nav-link">
+            Contact
+          </button>
+        </li>
+      </ul>
     </div>
   );
 };
 
 export default Navbar;
-
--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
+    fontFamily:{
+      rubix: ["Rubik", "sans-serif"],
+      poppins:["Poppins", "sans-serif"]
+    }
   },
   plugins: [],
 }
