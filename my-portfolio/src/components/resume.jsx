f414e810588e4d1c4460f84e0fa83f464fe71ff8
Ananya Biswas <153214834+ananyab1909@users.noreply.github.com>
2024-08-13 17:54:42 +0530

    Updated resume.jsx

--- my-portfolio/src/components/resume.jsx
+++ my-portfolio/src/components/resume.jsx
@@ -5,7 +5,7 @@ const Resume = () => {
     <div className="mt-10 md:mt-30 sm:mt-30 w-full md:flex sm:flex md:text-left sm:text-left"> 
       <button
         className="mt-10 md:mt-20 sm:mt-0 lg:mt-0 xl:-mt-10 bg-[#191825] p-3 font-semibold text-xm md:text-xs lg:text-xl border rounded shadow-md text-white hover:bg-white hover:text-black transition duration-300 ml-10 lg:ml-60 xl:ml-80"
-        onClick={() => window.open("https://drive.google.com/file/d/1NCvdKWh741Ceiw1-EmtSBgAooK6j8Ebf/view?usp=drivesdk ")}
+        onClick={() => window.open("https://drive.google.com/file/d/1JHAfO-WJtEMm8LET3LP0fiZndxSHBIOy/view?usp=sharing")}
       >
         View Resume
       </button>
